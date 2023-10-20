58e4018acce4a83a18efdbb803111ba945b0a3e3
petrakalulu <petrakalulu001@gmail.com>
2023-10-19 19:42:23 -0700

    Simple shell project

--- help_builtins1.c
+++ help_builtins1.c
@@ -1,21 +1,31 @@
 #include "shell.h"
 
-void display_help_message(char *msgs[], int size);
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
 
 /**
  * help_all - Displays all possible builtin shellby commands.
  */
 void help_all(void)
 {
-char *msgs[] = {
-"Shellby\nThese shell commands are defined internally.\n",
-"Type 'help' to see this list.\nType 'help name' to find out more about the function 'name'.\n\n",
-"  alias     \talias [NAME[='VALUE'] ...]\n  cd    \tcd [DIRECTORY]\n",
-"  exit      \texit [STATUS]\n  env       \tenv\n",
-"  setenv    \tsetenv [VARIABLE] [VALUE]\n  unsetenv\tunsetenv [VARIABLE]\n"
-};
+	char *msg = "Shellby\nThese shell commands are defined internally.\n";
 
-display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "Type 'help' to see this list.\nType 'help name' to find ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "out more about the function 'name'.\n\n  alias   \t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "alias [NAME[='VALUE'] ...]\n  cd    \tcd   ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "[DIRECTORY]\n  exit    \texit [STATUS]\n  env     \tenv";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n  setenv  \tsetenv [VARIABLE] [VALUE]\n  unsetenv\t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "unsetenv [VARIABLE]\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
@@ -23,13 +33,21 @@ display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
  */
 void help_alias(void)
 {
-char *msgs[] = {
-"alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n",
-"\n\talias: Prints a list of all aliases, one per line, in the format NAME='VALUE'.\n",
-"\talias NAME='VALUE' [...]: Defines an alias for each NAME whose VALUE is given.\n"
-};
+	char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n";
 
-display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n\talias: Prints a list of all aliases, one per line, in ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "the format NAME='VALUE'.\n\talias name [name2 ...]:prints";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " the aliases name, name2, etc. one per line, in the ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Defines";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " an alias for each NAME whose VALUE is given. If NAME ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "is already an alias, replace its value with VALUE.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
@@ -37,13 +55,19 @@ display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
  */
 void help_cd(void)
 {
-char *msgs[] = {
-"cd: cd [DIRECTORY]\n\tChanges the current directory of the process to DIRECTORY.\n",
-"\tIf no argument is given, the command is interpreted as cd $HOME.\n",
-"\tThe environment variables PWD and OLDPWD are updated after a change of directory.\n"
-};
+	char *msg = "cd: cd [DIRECTORY]\n\tChanges the current directory of the";
 
-display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " process to DIRECTORY.\n\n\tIf no argument is given, the ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "command is interpreted as cd $HOME. If the argument '-' is";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " given, the command is interpreted as cd $OLDPWD.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tThe environment variables PWD and OLDPWD are updated ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "after a change of directory.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
@@ -51,12 +75,15 @@ display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
  */
 void help_exit(void)
 {
-char *msgs[] = {
-"exit: exit [STATUS]\n\tExits the shell.\n",
-"\tThe STATUS argument is the integer used to exit the shell.\n"
-};
+	char *msg = "exit: exit [STATUS]\n\tExits the shell.\n\n\tThe ";
 
-display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "STATUS argument is the integer used to exit the shell.";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " If no argument is given, the command is interpreted as";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " exit 0.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
 
 /**
@@ -64,24 +91,11 @@ display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
  */
 void help_help(void)
 {
-char *msgs[] = {
-"help: help\n\tSee all possible Shellby builtin commands.\n",
-"\n\thelp [BUILTIN NAME]\n\tSee specific information on each builtin command.\n"
-};
+	char *msg = "help: help\n\tSee all possible Shellby builtin commands.\n";
 
-display_help_message(msgs, sizeof(msgs) / sizeof(msgs[0]));
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n      help [BUILTIN NAME]\n\tSee specific information on each ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "builtin command.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
-
-/**
- * display_help_message - Utility function to display messages.
- * @msgs: An array of message strings.
- * @size: Number of strings in the msgs array.
- */
-void display_help_message(char *msgs[], int size)
-{
-for (int i = 0; i < size; i++)
-{
-write(STDOUT_FILENO, msgs[i], _strlen(msgs[i]));
-}
-}
-
